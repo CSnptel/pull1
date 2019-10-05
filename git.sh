c4d5692650e745e3662683ac5f8cb8445734f173
shreyansh <shreyansh@cse.iitb.ac.in>
2019-10-06 00:41:25 +0530

    added PR $(($i))

--- git.sh
+++ git.sh
@@ -7,6 +7,8 @@ while [ $i -lt 4 ]
 do
 
 
-touch super$i && echo 'My $i PR for hacktoberfest' > super$i && git add . && git commit -m 'added PR $i' && git push origin himesh
+touch super$i && echo 'My $i PR for hacktoberfest' > super$i && git add . && git commit -m 'added PR $(($i))' && git push origin himesh 
+echo 'CSnptel\n' | git.sh
+
 i=$(($i+1))
 done
